dc66f068064fae868aee80aaae96923a16d59982
Mateo Sanabria <37130201+Teo813@users.noreply.github.com>
2023-10-02 11:15:26 -0400

    Adding Admin controls to Doorman

--- src/app_doorman.py
+++ src/app_doorman.py
@@ -5,7 +5,8 @@ import openai
 import time
 import os
 import re
-import jsonify
+import jsonify              
+import json
 import pandas               as pd
 from doorman_functions      import *
 from pathlib                import Path
@@ -17,7 +18,8 @@ from slackeventsapi         import SlackEventAdapter
 app = Flask(__name__)
 env_path = Path('.')/'.env'
 load_dotenv(dotenv_path=env_path)
-
+with open("adminKeys.json", "r") as file:
+    loaded_keys = json.load(file)
 # Load OpenAI, BotID, Slack Token and Slack Events Token
 openai.api_key = token=os.environ['CHAT_TOKEN']
 doorman_id = token=os.environ['DOORMAN_ID']
@@ -65,14 +67,61 @@ def message(payload):
 @app.route('/doorman/addnewgroup', methods=['GET','POST'])
 def addingGroup():
     data = request.form
+    user = data.get('user_id')
+    if adminAuth(user) is False:
+        return "I appreciate your attempt to fix up the question set but you aren't authorized"
     text = data.get('text')
     trigger_id = request.form['trigger_id']
     open_addition_modal(trigger_id)
     return '', 200
 
+@app.route('/doorman/claimadmin', methods=['GET','POST'])
+def adminClaim():
+    with open('adminKeys.json', "r") as file:
+            user_ids = json.load(file)
+    if not user_ids:
+        data = request.form
+        user = data.get('user_id')
+        adminAdd(user) 
+    else:
+        return f"Admin slot has already been claimed, please ask an admin to promote you.", 200
+    return 'Welcome to the Admin Team', 200
+
+@app.route('/doorman/addadmin', methods=['GET','POST'])
+def adminAddition():
+    data=request.form
+    user = data.get('user_id')   
+    text = data.get('text') 
+    if adminAuth(user) is False:
+        return("Only admins are allowed to promote users")
+    with open('adminKeys.json', "r") as file:
+            user_ids = json.load(file)
+    if text in user_ids:
+        return f'{text} is already an admin'
+    adminAdd(text)
+    return f'User {text} added to admins', 200
+
+@app.route('/doorman/removeadmin', methods=['GET','POST'])
+def adminRemoval():
+    data=request.form
+    user = data.get('user_id')   
+    text = data.get('text') 
+    if adminAuth(user) is False:
+        return("Only admins are allowed to demote users")
+    with open('adminKeys.json', "r") as file:
+            user_ids = json.load(file)
+    if text not in user_ids:
+        return f'{text} is not an admin'
+    adminRemove(text)
+    
+    return f'User {text} removed from admins', 200
+
 @app.route('/doorman/rmgroup', methods=['GET','POST'])
 def questionSetRemove():
     data=request.form
+    user = data.get('user_id')
+    if adminAuth(user) is False:
+        return "I appreciate your attempt to fix up the question set but you aren't authorized"
     text = data.get('text')
     trigger_id = request.form['trigger_id']
     open_deletion_modal(trigger_id)
@@ -119,6 +168,45 @@ def slack_interactive():
             new_modal = generate_modal_from_csv()
             response = update_slack_modal(data['trigger_id'],data['view']['id'], new_modal)
     return '', 200
+
+def adminAuth(key_to_check, filename="adminKeys.json"):
+    try:
+        with open(filename, "r") as file:
+            admin_keys = json.load(file)
+            return key_to_check in admin_keys
+    except FileNotFoundError:
+        print(f"File {filename} not found!")
+        return False
+    
+def adminAdd(user, filename="adminKeys.json"):
+    """Add a new user ID to the specified JSON file without overwriting existing IDs."""
+    # Load existing user IDs, if any
+    try:
+        with open(filename, "r") as file:
+            user_ids = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        user_ids = []
+    # Add the new user ID if it's not already in the list
+    if user not in user_ids:
+        user_ids.append(user)
+        # Save the updated list back to the file
+        with open(filename, "w") as file:
+            json.dump(user_ids, file)
+
+def adminRemove(user, filename="adminKeys.json"):
+    """Remove a new user ID to the specified JSON file without overwriting existing IDs."""
+    # Load existing user IDs, if any
+    try:
+        with open(filename, "r") as file:
+            user_ids = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        user_ids = []
+    # Add the new user ID if it's not already in the list
+    if user in user_ids:
+        user_ids.remove(user)
+        # Save the updated list back to the file
+        with open(filename, "w") as file:
+            json.dump(user_ids, file)
 
 # Surely we can do something better than this 
 def wakeUp():
